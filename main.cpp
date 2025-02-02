cf58cfa6410d34923c966b0c3b35a236ebeb98b6
Fatih Karakurt <1242775-FatihKarakurt@users.noreply.replit.com>
2025-02-02 21:28:59 +0000

    Assistant checkpoint: Remove duplicate main function from main.cpp
    Assistant generated file changes:
    - main.cpp: Remove duplicate main function
    
    ---
    
    User prompt:
    
    fix this error
    /nix/store/bgcaxhhxswzvmxjbbgvvaximm5hwghz1-binutils-2.41/bin/ld: /tmp/main-23de69.o: in function `std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)':
    /home/runner/workspace/main.cpp:10: multiple definition of `main'; /tmp/tests-4cd812.o:/home/runner/workspace/tests.cpp:77: first defined here
    clang-15: error: linker command failed with exit code 1 (use -v to see invocation)
    make: *** [Makefile:10: main] Error 1

--- main.cpp
+++ main.cpp
@@ -6,11 +6,3 @@
 // Function declarations
 void testLRUCache();
 void multithreadedTest();
-
-int main() {
-    std::cout << "Running LRU Cache tests...\n";
-    testLRUCache();
-    multithreadedTest();
-    std::cout << "All tests done!\n";
-    return 0;
-}
